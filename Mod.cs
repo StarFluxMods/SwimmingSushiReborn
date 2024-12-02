788550a7a6a1d2f417cbed9528aaf1c3badbaa15
Lachlan Leone <>
2024-12-01 17:38:16 -0800

    Reference Generation

--- Mod.cs
+++ Mod.cs
@@ -3,10 +3,7 @@ using KitchenLib.Logging.Exceptions;
 using KitchenMods;
 using System.Linq;
 using System.Reflection;
-using KitchenData;
-using KitchenLib.Event;
 using KitchenLib.Interfaces;
-using KitchenLib.References;
 using SwimmingSushiReborn.Patches;
 using SwimmingSushiReborn.Utilies;
 using UnityEngine;
@@ -14,7 +11,7 @@ using KitchenLogger = KitchenLib.Logging.KitchenLogger;
 
 namespace SwimmingSushiReborn
 {
-    public class Mod : BaseMod, IModSystem, IAutoRegisterAll
+    public class Mod : BaseMod, IModSystem , IAutoRegisterAll
     {
         public const string MOD_GUID = "com.starfluxgames.swimmingsushireborn";
         public const string MOD_NAME = "Swimming Sushi Reborn";
@@ -51,30 +48,7 @@ namespace SwimmingSushiReborn
             Bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).FirstOrDefault() ?? throw new MissingAssetBundleException(MOD_GUID);
             Logger = InitLogger();
 
-            Events.BuildGameDataEvent += (s, args) =>
-            {
-                /*
-                SetupTwoWayProcesses(GDOReferences.Countertop, GDOReferences.RollingMat, new Appliance.ApplianceProcesses
-                {
-                    Process = GDOReferences.Roll,
-                    Speed = 0.75f,
-                    Validity = ProcessValidity.Generic,
-                });
-                */
-            };
-        }
-
-        // Copies all processes from SourceAppliance to TargetAppliance, and adds an additional process to SourceAppliance
-        private void SetupTwoWayProcesses(Appliance sourceAppliance, Appliance targetAppliance, Appliance.ApplianceProcesses process)
-        {
-            foreach (var sourceProcess in sourceAppliance.Processes.Where(sourceProcess => targetAppliance.Processes.All(applianceProcess => applianceProcess.Process != sourceProcess.Process)))
-            {
-                targetAppliance.Processes.Add(sourceProcess);
-            }
-            if (sourceAppliance.Processes.All(applianceProcess => applianceProcess.Process != process.Process))
-            {
-                sourceAppliance.Processes.Add(process);
-            }
+            // GenerateGDOReferences(Assembly.GetExecutingAssembly(), Path.Combine(Application.persistentDataPath, "GeneratedReferences.cs"));
         }
     }
 }
---
+++ Utilies/RefGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using KitchenLib.Customs;
+using UnityEngine;
+
+namespace SwimmingSushiReborn.Utilies
+{
+    public class RefGenerator
+    {
+        
+
+        private static Type GetGDOType(Type type)
+        {
+            return type.Namespace.Contains("KitchenLib") ? type : GetGDOType(type.BaseType);
+        }
+
+        private static void GenerateGDOReferences(Assembly assembly, string file, string prefix = "", string protectionLevel = "public")
+        {
+            List<string> lines = new List<string>();
+            Dictionary<string, List<string>> categories = new Dictionary<string, List<string>>();
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(CustomGameDataObject).IsAssignableFrom(type) && !type.IsAbstract)
+                {
+                    if (!categories.ContainsKey(GetGDOType(type).Name.Replace("Custom", "")))
+                    {
+                        categories.Add(GetGDOType(type).Name.Replace("Custom", ""), new List<string>());
+                    }
+                    
+                    categories[GetGDOType(type).Name.Replace("Custom", "")].Add($"{protectionLevel} static {GetGDOType(type).Name.Replace("Custom", "")} {prefix}{type.Name} => ({GetGDOType(type).Name.Replace("Custom", "")})GDOUtils.GetCustomGameDataObject<{type.Name}>().GameDataObject;");
+                    
+                }
+            }
+
+            foreach (string category in categories.Keys)
+            {
+                lines.Add("#region " + category);
+                foreach (string codeLine in categories[category])
+                {
+                    lines.Add(codeLine);
+                }
+
+                lines.Add("#endregion");
+                lines.Add("");
+            }
+
+            File.WriteAllLines(file, lines.ToArray());
+            Application.Quit();
+        }
+    }
+}
