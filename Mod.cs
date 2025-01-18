9da77fe8628a0019b7bbf296e6cffc1abeb11e20
Lachlan Leone <lachlan@starfluxgames.com>
2025-01-18 20:07:58 +1100

    0.1.3

--- Mod.cs
+++ Mod.cs
@@ -29,7 +29,7 @@ namespace SwimmingSushiReborn
     {
         public const string MOD_GUID = "com.starfluxgames.swimmingsushireborn";
         public const string MOD_NAME = "Swimming Sushi Reborn";
-        public const string MOD_VERSION = "0.1.1";
+        public const string MOD_VERSION = "0.1.3";
         public const string MOD_AUTHOR = "StarFluxGames";
         public const string MOD_GAMEVERSION = ">=1.2.0";
 
